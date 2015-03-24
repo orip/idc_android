1f05cb0656030dea37b53ea9c54ac7cedc381f36
Ori Peleg <nospam@orip.org>
2015-03-24 16:11:19 +0200

    onItemClicked listener

--- app/src/main/java/example/org/fooapp/ChooseMemeActivity.java
+++ app/src/main/java/example/org/fooapp/ChooseMemeActivity.java
@@ -1,89 +1,68 @@
 package example.org.fooapp;
 
-import android.content.Context;
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.BaseAdapter;
 import android.widget.GridView;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 
 public class ChooseMemeActivity extends ActionBarActivity {
 
-    private ImagesAdapter mImagesAdapter;
+    private int[] mDrawableIds = new int[]{
+            R.drawable.success_kid,
+            R.drawable.socially_awkward_penguin,
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_choose_meme);
 
-        mImagesAdapter = new ImagesAdapter(this, new int[]{
-                R.drawable.success_kid,
-                R.drawable.socially_awkward_penguin
-        });
-
         GridView grid = (GridView) findViewById(R.id.grid);
-        grid.setAdapter(mImagesAdapter);
+        grid.setAdapter(new ImagesAdapter());
+        grid.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                Log.d("ChooseMemeActivity", "Image clicked, index=" + i + ", id=" + mDrawableIds[i]);
+            }
+        });
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_choose_meme, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-
-    private static class ImagesAdapter extends BaseAdapter {
-        private final Context mContext;
-        private final int[] mDrawableIds;
-
-        private ImagesAdapter(Context context, int[] drawableIds) {
-            mContext = context;
-            this.mDrawableIds = drawableIds;
-        }
-
+    /**
+     * Very simple adapter, can be optimized
+     */
+    private class ImagesAdapter extends BaseAdapter {
         @Override
         public int getCount() {
             return mDrawableIds.length;
         }
 
         @Override
-        public Object getItem(int i) {
-            return null;
-        }
-
-        @Override
-        public long getItemId(int i) {
-            return 0;
-        }
-
-        @Override
         public View getView(int i, View convertView, ViewGroup viewGroup) {
             // TODO: can optimize this with convertView
-            ImageView result = new ImageView(mContext);
+            ImageView result = new ImageView(ChooseMemeActivity.this);
             result.setScaleType(ImageView.ScaleType.CENTER_CROP);
             result.setPadding(12, 12, 12, 12);
             result.setImageResource(mDrawableIds[i]);
             return result;
+        }
+
+        // Unused right now
+        @Override
+        public Object getItem(int i) {
+            return null;
+        }
+
+        // Unused right now
+        @Override
+        public long getItemId(int i) {
+            return 0;
         }
     }
 }
